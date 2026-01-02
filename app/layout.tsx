760bd25bf9d04ccf60d472f571fe8bb0f741cd1b
Mathew-ak <eleojojames23@gmail.com>
2026-01-02 19:56:27 +0100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const _geistMono = Geist_Mono({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: 'v0 App',
   description: 'Created with v0',
-  generator: 'v0.app',
+  generator: 'Elevate',
   icons: {
     icon: [
       {
